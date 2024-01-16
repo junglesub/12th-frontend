80669cbee87ca34bd813b624afde6a8bf3f684e8
junglesub <junglesubmarine@gmail.com>
2024-01-16 07:47:46 +0000

    유정섭 이름 추가

--- members.js
+++ members.js
@@ -1,8 +1,8 @@
 const members = [
   {
-    name: "이름",
-    link: "https://likelionhgu.github.io/12th-frontend/",
-    github: "https://github.com/LikeLionHGU/12th-frontend",
+    name: "유정섭",
+    link: "https://google.com",
+    github: "https://github.com/junglesub",
     image: "https://placehold.co/400x250",
   },
 ];
