b43c23c734074e31c2f6aeb0091bb35ac0f53749
Jang <hansung.j1106@gmail.com>
2024-01-18 17:32:54 +0900

    장한성 정보 추가

--- members.js
+++ members.js
@@ -1,8 +1,8 @@
 const members = [
   {
     name: "장한성",
-    link: "https://likelionhgu.github.io/12th-frontend/",
-    github: "https://github.com/HansungJang/Lion_HGU_2024_Jang_profile",
+    link: "https://hansungjang.github.io/Lion_HGU_2024_Jang_profile/",
+    github: "https://github.com/HansungJang",
     image:
       "https://cdn.imweb.me/upload/S201910012ff964777e0e3/62f9a36ea3cea.jpg",
   },
