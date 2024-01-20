768cc35e0fdfca700d7af85079c76b1ede1a1565
hwan129 <hoanshim01@handong.ac.kr>
2024-01-20 22:16:41 +0900

    error: merge error

--- members.js
+++ members.js
@@ -1,69 +1,100 @@
 const members = [
   {
-    name: "김예지",
+    name: "☃️ 김예지",
     link: "https://skwldwld.github.io/p0",
     github: "https://github.com/skwldwld",
     image: "https://skwldwld.github.io/p0/img/favorite/dog.JPG",
+    title: "",
+    githuNickname: "",
+    selfPR: "",
   },
   {
-    name: "김하은",
+    name: "🎀 김하은",
     link: "https://google.com",
     github: "https://github.com/HAEOII",
     image:
       " https://haeoii.github.io/basic-setting/img/KakaoTalk_Photo_2024-01-07-14-47-27%20001jpeg.jpeg",
+    title: "",
+    githuNickname: "",
+    selfPR: "",
   },
   {
-    name: "한선규",
+    name: "⚽️ 한선규",
     link: "https://sungyu0309.github.io/Profile/",
     github: "https://github.com/sungyu0309",
     image:
       "https://images.unsplash.com/photo-1503614472-8c93d56e92ce?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
+    title: "",
+    githuNickname: "",
+    selfPR: "",
   },
   {
-    name: "심성환",
+    name: "🌊 심성환",
     link: "https://hwan129.github.io/myprofile/",
     github: "https://github.com/hwan129",
     image: "https://hwan129.github.io/myprofile/photos/photo4.jpg",
+    title: "",
+    githuNickname: "",
+    selfPR: "",
   },
   {
-    name: "임종현",
+    name: "🍓 임종현",
     link: "https://dkrehd0519.github.io/ll-test/",
     github: "https://github.com/dkrehd0519",
     image: "https://dkrehd0519.github.io/ll-test/image/me.jpg",
+    title: "",
+    githuNickname: "",
+    selfPR: "",
   },
   {
-    name: "장한성",
+    name: "🤖 장한성",
     link: "https://bit.ly/423tfqk",
     github: "https://github.com/HansungJang",
     image:
       "https://img.freepik.com/premium-photo/a-potato-with-sunglasses-and-a-face-that-says-potato_916191-1794.jpg?w=1060",
+    title: "",
+    githuNickname: "",
+    selfPR: "",
   },
   {
-    name: "이한나",
+    name: "🐼 이한나",
     link: "https://leehannaa.github.io/likeLion-profile/", // 추후 배포한 사이트 링크
     github: "https://github.com/LeeHannaa",
     image: "https://avatars.githubusercontent.com/u/103620720?v=4",
+    title: "Think different",
+    githuNickname: "Leehannaa",
+    selfPR:
+      "다른 생각으로 발전해나가는 개발자, 이한나입니다. \n3주간의 기간 동안 협력하여 우리만의 프로젝트를 완성시켜 나가겠습니다.🔥\n‘당당한 개발자, 새로운 개발자’ 모습에 한 발자국 더 가까워져 있는 모습을 기대합니다.👊🏻",
   },
   {
-    name: "최예라",
+    name: "🐥 최예라",
     link: "https://yearachoi.github.io/Introduction-Mini-Project/",
     github: "https://github.com/YearaChoi",
     image:
       "https://yearachoi.github.io/Introduction-Mini-Project/images/myphoto.jpeg",
+    title: "",
+    githuNickname: "",
+    selfPR: "",
   },
   {
-    name: "박은주 ",
+    name: "🐿️ 박은주 ",
     link: "https://ejpark43.github.io/likeLion_assignment1/",
     github: "https://github.com/ejPark43",
     image:
       "https://ejpark43.github.io/likeLion_assignment1/damgomDance_gif.gif",
+    title: "",
+    githuNickname: "",
+    selfPR: "",
   },
   {
-    name: "김하영",
+    name: "🎵 김하영",
     link: "https://hayoung04.github.io/Self-introduction/",
     github: "https://github.com/Hayoung04",
     image:
       "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20160218_67%2Fj23486_14557676251908Cd6S_JPEG%2F1430311276.57.jpg&type=sc960_832",
+    title: "",
+    githuNickname: "",
+    selfPR: "",
   },
 ];
 
@@ -79,7 +110,7 @@ members.map((memberData) => {
     .querySelector(".info-github > a")
     .setAttribute("href", memberData.github); // 깃헙주소 변경
   node
-    .querySelector(".card-banner > a > img")
+    .querySelector(".card-banner > img")
     .setAttribute("src", memberData.image);
 
   // 사이트 주소 링크(href) 와 Text 를 바꾸기 위해
@@ -95,5 +126,15 @@ members.map((memberData) => {
       el.innerText = memberData.link;
   });
 
+  node.querySelector("modal_img > img").setAttribute("src", memberData.image);
+  node.querySelector("modal_title > h2").innerText = memberData.title;
+  node.querySelector("modal_selfPR > p").innerText = memberData.selfPR;
+
   membersNode.appendChild(node); // 만들어진 node 를 다시 추가
 });
+
+function openModal() {
+  const modal = document.querySelector(".modal");
+  console.log("click");
+  modal.style.display = "flex";
+}
